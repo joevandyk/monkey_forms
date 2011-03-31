5b0e813f06dcaa29c6f43d1b8b468aff53e2eda1
Joe Van Dyk <joe@fixieconsulting.com>
2011-03-30 17:32:13 -0700

    wip

--- lib/monkey_forms.rb
+++ lib/monkey_forms.rb
@@ -13,7 +13,10 @@ module MonkeyForms
 
     module InstanceMethods
       def initialize attrs={}
-        @attributes = {}
+        # TODO handle properly (encode, gzip, etc)
+        @cookie     = attrs.delete(self.class.cookie_name.to_sym) || {}
+        @attributes = @cookie
+
         attrs.each do |name, value|
           @attributes[name.to_sym] = value
         end
@@ -21,6 +24,17 @@ module MonkeyForms
     end
 
     module ClassMethods
+      attr_reader :cookie_name
+
+      def form_storage storage_type, options={}
+        case storage_type
+        when :cookie
+          @cookie_name = options.delete(:name) || 'monkey_form_cookie'
+        else
+          raise ArgumentError.new "Unknown storage type picked"
+        end
+      end
+
       def form_attributes *attrs
         attrs.each do |attr|
           define_method attr do
--- test/test_monkey_forms.rb
+++ test/test_monkey_forms.rb
@@ -2,18 +2,19 @@ require 'rubygems'
 require 'minitest/autorun'
 require 'monkey_forms'
 
-class TestMonkeyForms < MiniTest::Unit::TestCase
-  class OrderForm
-    include MonkeyForms::Form
-    form_attributes :name, :email
+class OrderForm
+  include MonkeyForms::Form
+  form_attributes :name, :email
+  form_storage :cookie, :name => 'order_cookie'
 
-    validates :email, :presence => true
+  validates :email, :presence => true
 
-    validation_scope :cart_errors do |scope|
-      scope.validates :name, :presence => true
-    end
+  validation_scope :cart_errors do |scope|
+    scope.validates :name, :presence => true
   end
+end
 
+class TestMonkeyForms < MiniTest::Unit::TestCase
   def test_basic
     o = OrderForm.new :name => "Joe", :email => "joe@tanga.com"
     assert_equal "Joe", o.name
@@ -37,4 +38,32 @@ class TestMonkeyForms < MiniTest::Unit::TestCase
     # Email is fine, valid? wasn't called
     assert_equal [], o.errors[:email]
   end
+
+  def test_unknown_storage_type
+
+  end
+
+  def test_basic_cookie
+    # Some Rails cookie thingy w/ email set
+    # TODO figure out how to properly test this with ActionPack's cookies
+    cookie = create_cookie :email => "joe@tanga.com"
+    o = OrderForm.new :name => "Joe", :order_cookie => cookie
+
+    assert_equal "Joe", o.name
+    assert_equal "joe@tanga.com", o.email
+  end
+
+  def test_form_overrides_cookie
+    cookie = create_cookie :email => "joe@tanga.com"
+    o = OrderForm.new :email => "joe@domain.com", :order_cookie => cookie
+
+    assert_equal "joe@domain.com", cookie[:email]
+    assert_equal "joe@domain.com", o.email
+  end
+
+  # TODO this should create an actual cookie
+  def create_cookie options
+    {}.merge!(options)
+  end
 end
+
